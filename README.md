78289ed4e7ac613ea62b1c6e382c04cfd7aa8702
su.ha <su.ha@cherry-solutions.com>
2024-08-02 14:12:53 +0700

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Spring boot template with Spring security 6
 ### Swagger: http://localhost:8080/swagger-ui/index.html
 
-change
+Use converter: Target object need @Convert
+example: **@Convert(converter = LocalDateConverter.class)**
--- src/main/java/com/example/helper/ResultHelper.java
+++ src/main/java/com/example/helper/ResultHelper.java
@@ -1,43 +1,81 @@
 package com.example.helper;
 
 import ch.qos.logback.core.spi.ErrorCodes;
+import com.example.convert.LocalDateConverter;
 import com.example.exception.ApplicationException;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 public class ResultHelper {
-    public <T> List<T> getResultList(Query query, Class<T> type) {
-        List<Object[]> datas = query.getResultList();
-        return map(type, datas);
 
-    }
-
-    private <T> List<T> map(Class<T> type, List<Object[]> datas) {
+    private static <T> List<T> map(Class<T> type, List<Object> records) {
         List<T> result = new LinkedList<>();
-        for (var data : datas) {
-            result.add(map(type, data));
+        for (Object rc : records) {
+            if (rc.getClass().isArray()) {
+                result.add(map(type, (Object[]) rc));
+            } else if (type.isAssignableFrom(rc.getClass())) {
+                @SuppressWarnings("unchecked")
+                T v = (T) rc;
+                result.add(v);
+            }
         }
         return result;
     }
 
-    private <T> T map(Class<T> type, Object[] tuple) {
-        List<Class<?>> tupleType = new ArrayList<>();
-
+    private static <T> T map(Class<T> type, Object[] tuple) {
+        List<Class<?>> tupleTypes = new ArrayList<>();
+        var x = type.getDeclaredFields();
         for (Field field : type.getDeclaredFields()) {
-            tupleType.add(field.getType());
+            tupleTypes.add(field.getType());
+        }
+        for (int i = 0; i < x.length; i++) {
+            var convertAnnotation = x[i].getAnnotation(jakarta.persistence.Convert.class);
+            if (convertAnnotation != null && tuple[i] instanceof Date date && convertAnnotation.converter().equals(LocalDateConverter.class)) {
+                tuple[i] = new LocalDateConverter().convertToEntityAttribute(date);
+            }
+
         }
         try {
-            Constructor<T> ctor = type.getConstructor(tupleType.toArray(new Class<?>[tuple.length]));
+            Constructor<T> ctor = type.getConstructor(tupleTypes.toArray(new Class<?>[tuple.length]));
             return ctor.newInstance(tuple);
-        } catch (Exception e) {
-            throw new ApplicationException(5002, e.getMessage(), e);
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException |
+                 SecurityException e) {
+            throw new ApplicationException(500, e.getMessage(), e);
         }
+    }
+
+    public static <T> List<T> getResultList(Query query, Class<T> type) {
+        @SuppressWarnings("unchecked")
+        List<Object> records = query.getResultList();
+        return map(type, records);
+    }
+
+    public static <T> T defaultIfNull(final T object, final T defaultValue) {
+        return object != null ? object : defaultValue;
+    }
+
+    public static <T> Set<T> getResultSet(Query query, Class<T> type) {
+        @SuppressWarnings("unchecked")
+        List<Object[]> records = query.getResultList();
+        return mapAsSet(type, records);
+    }
+
+    private static <T> Set<T> mapAsSet(Class<T> type, List<Object[]> records) {
+        Set<T> result = new HashSet<>();
+        for (Object[] rc : records) {
+            result.add(map(type, rc));
+        }
+        return result;
     }
 }
